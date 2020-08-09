4cf63dd3b85ba42125a65c680f9afc8595bc4a5f
Vidit Shah <vidit0210@outlook.com>
2020-08-09 23:22:13 +0530

    Expression

--- src/Hello.java
+++ src/Hello.java
@@ -1,7 +1,14 @@
+import java.net.SocketTimeoutException;
+import java.util.SortedMap;
+
 public class Hello {
 
     public static void main(String[] args) {
         System.out.println("Hello Vidit!");
+        int myFirstNumber = 10+91;
+        System.out.println(myFirstNumber);
+        System.out.println();
+
 
     }
 }
