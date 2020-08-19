09f685896abef8186f60fb166b7b72986eef2a5c
Vidit Shah <vidit0210@outlook.com>
2020-08-19 20:10:26 +0530

    qw

--- CharAndBoolean/src/com/company/Main.java
+++ CharAndBoolean/src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         char myChar = 'D';
-        char myUniCode = '\u0044';
+        char myUniCode = '\u00A9';
         System.out.println(myUniCode);
 
     }
