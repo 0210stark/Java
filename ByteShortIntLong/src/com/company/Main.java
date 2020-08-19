4ac3501cb8057826be6ec4104ab4b10df9c10efe
Vidit Shah <vidit0210@outlook.com>
2020-08-19 18:58:51 +0530

    shortValue

--- ByteShortIntLong/src/com/company/Main.java
+++ ByteShortIntLong/src/com/company/Main.java
@@ -3,7 +3,25 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        
+        //Intetger Miin and Max value
+        int maxValue  = Integer.MAX_VALUE;
+        int minValue = Integer.MIN_VALUE;
+        System.out.println("Min : " + minValue);
+        System.out.println("Max : "+ maxValue);
+
+        //Byte Values..
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
+        System.out.println(myMinByteValue);
+        System.out.println(myMaxByteValue);
+
+        //Short Values
+
+        short myShortValue = Short.MIN_VALUE;
+        short myShorMaxValue = Short.MAX_VALUE;
+        System.out.println(myShortValue);
+        System.out.println(myShorMaxValue);
+
+
     }
 }
