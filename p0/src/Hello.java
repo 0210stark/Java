8b5738f838688f21eaba195176f8122704736ceb
Vidit Shah <vidit0210@outlook.com>
2020-08-19 18:49:48 +0530

    primitive

--- p0/src/Hello.java
+++ p0/src/Hello.java
@@ -2,7 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         int myFirstNumber = 10;
         int mySecondNumber = 12;
-        int myThirdNumber  = 6;
+        int myThirdNumber  = myFirstNumber * 3;
         int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
 
         System.out.println(myTotal);
