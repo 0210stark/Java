bc279b006f47b710ef13c7976c28c536648fafb0
Vidit Shah <vidit0210@outlook.com>
2020-08-22 04:13:53 +0530

    cconversion

--- Section4CodingExcercise/src/SpeeDConverter.java
+++ Section4CodingExcercise/src/SpeeDConverter.java
@@ -1,10 +1,11 @@
 public class SpeeDConverter {
     public static void main(String[] args) {
-        System.out.println(toMilesPerHour(25.42d));
+        System.out.println(toMilesPerHour(1.5));
     }
     public static  long toMilesPerHour(double kilometersPerHour){
         if(kilometersPerHour<0)
             return  -1;
-        return (int) (kilometersPerHour/0.621371d );
+        double res =  (kilometersPerHour * 0.621371 );
+        return  Math.round(res);
     }
 }
