52e39c0c48e888878f9c448fe382fc3b1b3120bf
Vidit Shah <vidit0210@outlook.com>
2020-08-22 01:31:26 +0530

    Abrevate

--- Section4/src/com/company/Main.java
+++ Section4/src/com/company/Main.java
@@ -14,18 +14,20 @@ public class Main {
 //        myVariable++;
 //        myVariable--;
 //        System.out.println("This is a  test" + myVariable);
-        boolean gameOver = true;
-        int score = 10000;
-        int levelCompleted =5;
-        if(score == 5000){
-            System.out.println("High Score");
-        }else if (score==9000){
-            System.out.println("Need More Score to pass the test!");
-        }else{
-            System.out.println("No condition met!");
+        //
+//        boolean gameOver = true;
+//        int score = 10000;
+//        int levelCompleted =5;
+//        if(score == 5000){
+//            System.out.println("High Score");
+//        }else if (score==9000){
+//            System.out.println("Need More Score to pass the test!");
+//        }else{
+//            System.out.println("No condition met!");
         }
 
+        
         }
 
-    }
+
 
