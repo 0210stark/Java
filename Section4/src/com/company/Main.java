02a5856ed9c098b13d76e5405319401f07ae3972
Vidit Shah <vidit0210@outlook.com>
2020-08-22 01:17:30 +0530

    ex2

--- Section4/src/com/company/Main.java
+++ Section4/src/com/company/Main.java
@@ -4,11 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
 	// A Mie to Kilometer
-        double Kilnmeters = (100 * 1.609344);
-        int highScore = 50;
-        if(highScore == 50){
-            System.out.println("This is an expression");
-        }
+//        double Kilnmeters = (100 * 1.609344);
+//        int highScore = 50;
+//        if(highScore == 50){
+//            System.out.println("This is an expression");
+
+        //Statements...
         
+        }
+
     }
 }
