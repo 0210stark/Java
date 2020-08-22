1caebfc42c63b444e9a77662f9015d11028f832c
Vidit Shah <vidit0210@outlook.com>
2020-08-22 16:43:28 +0530

    KilometeresPerhour

--- Section4CodingExcercise/src/SpeeDConverter.java
+++ Section4CodingExcercise/src/SpeeDConverter.java
@@ -1,11 +1,20 @@
 public class SpeeDConverter {
+
+
     public static void main(String[] args) {
         System.out.println(toMilesPerHour(1.5));
     }
+
+
+
     public static  long toMilesPerHour(double kilometersPerHour){
         if(kilometersPerHour<0)
             return  -1;
         double res =  (kilometersPerHour * 0.621371 );
         return  Math.round(res);
+    }
+
+    public static void mainPrintDetails(double kilometersperHouts){
+
     }
 }
